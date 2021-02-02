2687d982c454544e43b4e0af934f705ad2fb48ce
Hongli Zhao <honglizhaobob@berkeley.edu>
2021-02-01 18:17:18 -0800

    discrd

--- sp21/navbar.js
+++ sp21/navbar.js
@@ -36,6 +36,10 @@ var html = `
                     <a class="nav-link" href="https://www.gradescope.com/courses/154589">Gradescope</a>
                 </li>
 
+                <li class="nav-item">
+                    <a class="nav-link" href="https://discord.gg/rdAwEtPGFF">Discord</a>
+                </li>
+
                 <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Resources
